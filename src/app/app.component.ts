8f7d6e3e5491dbe79e33ca8afeecbd2ac9afe679
vrushali <kamblevrushali182@gmail.com>
2024-05-08 04:27:31 +0530

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,NgModule,OnInit } from '@angular/core';
+import { Component,NgModule,OnInit,OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,25 +12,94 @@ export class AppComponent implements OnInit{
   searchText = '';
   searchDetail = '';
   searchPrice! : number;
+  paginationArr = [1];
+  paginationIndex =1;
+  paginationFiltered! :Product[];
+  hightlightButton : number =1;
+
+  numberOfLiTags: number = 5; // Set the desired number of <li> tags 18 [1,2,3,4,5,6,7,8,9]  2  3 6   3 6 9  4 9 12
+
+  @ViewChild('preButton') preButton!: ElementRef ;
+  @ViewChild('nextButton') nextButton! : ElementRef
+
+  previousButton(buttonName : string){
+    if(buttonName == 'pre' && this.hightlightButton>1){
+      this.selectPage(this.hightlightButton-1)
+    }
+  
+    if(buttonName == 'next' && this.hightlightButton<this.paginationArr.length){
+      this.selectPage(this.hightlightButton+1)
+    }
+    if(buttonName == 'disableFalse' && this.hightlightButton ==1 )
+      {
+        this.preButton.nativeElement.disabled = true;
+        this.nextButton.nativeElement.disabled = false;
+      }
+    else  if(buttonName == 'disableFalse' && this.hightlightButton == this.paginationArr.length){
+        this.nextButton.nativeElement.disabled = true;
+        this.preButton.nativeElement.disabled = false;
+      }
+    else if(buttonName == 'pre' && this.hightlightButton ==1 ){
+      this.preButton.nativeElement.disabled = true;
+    }
+      else{
+        this.nextButton.nativeElement.disabled = false;
+        this.preButton.nativeElement.disabled = false;
+      }
+  }
+
+  pagination(){
+    this.getRange();
+    this.paginationArr = [1];
+    for(let i=2;i<= this.numberOfLiTags ;i++){
+      this.paginationArr.push(i);
+      
+    }
+    this.paginationFiltered = this.filtered.slice(0,3);
+    this.previousButton('disableFalse')
+  }
+  
+
+  selectPage(index : number){
+    this.paginationFiltered = this.filtered.slice((index-1)*3 , index*3);
+    this.hightlightButton = index;
+    this.previousButton('disableFalse')
+  }
+
+  getRange() {
+    if(this.filtered.length/3 > 0 && this.filtered.length%3 == 0){
+      this.numberOfLiTags = this.filtered.length/3;
+    console.log(this.numberOfLiTags,"here page")
+    }
+    else
+    this.numberOfLiTags = this.filtered.length/3 + 1;
+  }
+  
 
 filtered! : Product[] ;
   filterProducts(){
     console.log("in search")
-    this.filtered = this.productList.filter(product =>
+    this.paginationFiltered=this.filtered = this.productList.filter(product =>
       product.name.toLowerCase().includes(this.searchText.toLowerCase())
     );
+    this.selectPage(1);
   }
 
   filterProductsDetail(){
-    this.filtered = this.productList.filter(product =>
-      product.details.toLowerCase().includes(this.searchText.toLowerCase())
+    console.log("in details")
+    this.paginationFiltered=this.filtered = this.productList.filter(product =>
+      product.details.toLowerCase().includes(this.searchDetail.toLowerCase())
     );
+    this.selectPage(1);
   }
 
-  // filterProducts(): void {
-  //   // filterProducts = this.p.filter(product =>
-  //   //   product.name.toLowerCase().includes(this.searchText.toLowerCase())
-  //   // );
+  filterProductsPrice(){
+    console.log("in price")
+    this.paginationFiltered=this.filtered = this.productList.filter(product =>
+      product.price.toString().includes(this.searchPrice.toString())
+    );
+    this.selectPage(1);
+  }
 
   productObj : Product = new Product ();
   productList : Product[] =[{
@@ -55,12 +124,14 @@ filtered! : Product[] ;
 },];
 
   ngOnInit(): void {
+    this.hightlightButton = 1;
     const localData = localStorage.getItem("products");
     if(localData!= null){
       this.productList = JSON.parse(localData);
       this.filtered =this.productList;
     }
-
+    this.pagination();
+    this.paginationIndex =2;
   }
 
   addNew(){
@@ -93,7 +164,8 @@ filtered! : Product[] ;
       localStorage.setItem('products',JSON.stringify(newA));
     }
     this.productObj = new Product();
-
+    this.filtered = this.productList;
+    this.pagination();
     
   }
   updateForm(){
@@ -128,7 +200,10 @@ this.shoFrom =false;
      const currentRecord = this.productList.findIndex(m => m.name === pro.name)
      this.productList.splice(currentRecord,1);
      localStorage.setItem('products',JSON.stringify(this.productList))
+       
     }
+    this.filtered = this.productList;
+    this.pagination();
     }
   
   constructor(){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ElementRef } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
